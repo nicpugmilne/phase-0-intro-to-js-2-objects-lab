f20f8d854c02a312979063e58700611ede385aca
Nicola Milne <nicolalouisemilne@gmail.com>
2022-05-18 11:40:32 -0600

    commit tree 29379914d3b26b3967911efef29a7715d7da0ace

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(array, key, value){
+    const newArray = {...array};
+    newArray.key = value;
+    return newArray;
+};
+
+console.log(employee);
+updateEmployeeWithKeyAndValue(employee, "name", "Sally");
